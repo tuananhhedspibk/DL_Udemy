0c3cffe90d1bd07421ee57552d42a5d010cacaf6
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2019-04-08 22:46:08 +0900

    Complete ann

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 # Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+dataset = pd.read_csv("Churn_Modelling.csv")
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
@@ -32,6 +34,21 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 classifier = Sequential()
-classifier.add(Dense(output_dim = 6, init = "uniform", activation="relu", input_dim = 11))
-classifier.add(Dense(output_dim = 6, init = "uniform", activation="relu"))
-classifier.add(Dense(output_dim = 1, init = "uniform", activation="sigmoid"))
+classifier.add(Dense(output_dim = 6,
+    init = "uniform", activation="relu", input_dim = 11))
+classifier.add(Dense(output_dim = 6,
+    init = "uniform", activation="relu"))
+classifier.add(Dense(output_dim = 1,
+    init = "uniform", activation="sigmoid"))
+
+# adam is stochastic gradient descent
+
+classifier.compile(optimizer = "adam",
+    loss = "binary_crossentropy", metrics = ["accuracy"])
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
